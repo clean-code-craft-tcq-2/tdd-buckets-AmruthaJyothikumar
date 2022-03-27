f166762cf29db376afcc88a203211ed67030c700
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-03-27 13:46:05 +0530

    Update CurrentRangeandSamples.c

--- CurrentRangeandSamples.c
+++ CurrentRangeandSamples.c
@@ -1,30 +1,74 @@
 #include <stdio.h>
 #include "CurrentRangeandSamples.h"
 
-currentrange getrange(int *CurrentSamples){
-  currentrange range;
-  range.min = findminvalue;
-  range.max = findmaxvalue;
-  range.count = getcountofcontinousrange;
-  return range;
+bool validateInput(int *Input , int length)
+{
+	int i;
+  bool validity;
+	for(i= 0; i < length; i++)
+	{
+		if(Input[i] >= 0)
+		{
+			sortInput(Input, length);			
+			validity =  true;
+		}	
+		else
+		{
+			validity =  false;
+		}
+	}
+  if(validity == true){
+    sortInput(Input, length);		
+  }
+  else{
+  }
+return validity;
 }
 
+void sortInput(int *Input , int length)
+{
+	int i , j , temp ;
+	for(i=0; i< length; i++)
+	{
+		for(j=i+1; j< length; j++)
+		{
+			if(Input[i] > Input[j])
+			{
+				temp = Input[i];
+				Input[i] = Input[j];
+				Input[j] = temp; 
+			}
+		}	
+	}
+}
 
-int getcountofcontinousrange(int *CurrentSamples){
-  int totallengthofinput;
-  int diff = 1;
-  int continuouscount;
-  totallengthofinput = sizeof(CurrentSamples) / sizeof(CurrentSamples[0]);
-  for(i=0; i<= totallengthofinput ;i++){
-     diff = CurrentSamples[i+1]- CurrentSamples[i];
-     if((diff == 0) || (diff == 1) || diff(==2)
-        {
-          continuouscount++;
-        }
-        else
-        {
-          continuouscount = 0;
-        }
+int findContinousCount(int *InputSamples, int NoofSamples)
+{
+    int i, j, SampleDiff;
+    if(validateInput(InputSamples, NoofSamples) == true)
+    {
+	    for(i = 0; i < NoofSamples; i++)
+	    {
+		  SampleDiff = (InputSamples[i+1] -  InputSamples[i]);
+		  if((SampleDiff == 0) || (SampleDiff == 1))
+		  {
+			  Continouscount++;
+	  	}
+		
+	    }
+	   return Continouscount;
     }
-  return continuouscount;
 }
+
+RangeReading GetCurrentRange( int *InputSamplesSorted)
+{
+ int ContinousCount = findContinousCount;
+	RangeReading rangeReading;
+	rangeReading.StartIndex= InputSamplesSorted[0];
+	rangeReading.EndIndex= InputSamplesSorted[ContinousCount-1];
+	rangeReading.NumOfReadingsInRange= ContinousCount;
+	printf(" %d - %d , %d\n",  rangeReading.StartIndex,rangeReading.EndIndex , rangeReading.NumOfReadingsInRange);	 
+	return rangeReading;
+}
+
+
