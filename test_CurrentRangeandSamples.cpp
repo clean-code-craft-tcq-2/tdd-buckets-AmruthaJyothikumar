c563a0fd638af7a6b13a670add5ceb0b87bf4dde
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-05 16:47:49 +0530

    Update test_CurrentRangeandSamples.cpp

--- test_CurrentRangeandSamples.cpp
+++ test_CurrentRangeandSamples.cpp
@@ -37,3 +37,36 @@ TEST_CASE("Case checks the given current reading for csv format construction") {
 
 }
   
+TEST_CASE("Checks for the current values for given ADC values from the array") {
+	
+	int ADCValues[] = {64, 512, 1140, 1220, 1560, 2048,4094};
+	int numberOfSamples = sizeof(ADCValues) / sizeof(ADCValues[0]);
+	int expectedCurrentSenseValues[] = {0, 1, 2, 3, 4, 5, 10};
+	int ADC_Resolution = 12;
+	int maxCurrentValue = 10;
+	int currentSenseValues[numberOfSamples];
+	int expectedStartIndex = 0, expectedEndIndex = 5 , expectedNumOfReadingsInRange = 6;
+	RangeReading rangeReading;
+	ChargingCurrentSenseValue(ADCValues, numberOfSamples, ADC_Resolution, maxCurrentValue, currentSenseValues);
+	 rangeReading = detectCurrentReadingAndFormatOutputInCSVFormat(expectedStartIndex,expectedEndIndex,expectedNumOfReadingsInRange);
+ 	 REQUIRE(rangeReading.StartIndex == expectedStartIndex);
+ 	 REQUIRE(rangeReading.EndIndex == expectedEndIndex);
+ 	 REQUIRE(rangeReading.NumOfReadingsInRange == expectedNumOfReadingsInRange);
+
+}
+
+TEST_CASE("Checks for the ignore the error when given ADC value is 4095") {
+	
+	int ADCValues[] = {4095};
+	int numberOfSamples = sizeof(ADCValues) / sizeof(ADCValues[0]);
+	int ADC_Resolution = 12;
+	int maxCurrentValue = 10;
+	int currentSenseValues[numberOfSamples];
+	int expectedADC_Resolution = 12, expectedmaxCurrentValue = 10 , expectednumberOfSamples = 1;
+	
+	ChargingCurrentSenseValue(ADCValues, numberOfSamples, ADC_Resolution, maxCurrentValue, currentSenseValues);
+	REQUIRE(ADC_Resolution == expectedADC_Resolution);
+  	REQUIRE(maxCurrentValue == expectedmaxCurrentValue);
+  	REQUIRE(numberOfSamples == expectednumberOfSamples);
+
+}
