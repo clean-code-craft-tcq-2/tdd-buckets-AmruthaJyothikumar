3d129d1643c2ba95263c696f2a56ca4f53b85df3
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-06 15:23:50 +0530

    Update CurrentRangeandSamples.h

--- CurrentRangeandSamples.h
+++ CurrentRangeandSamples.h
@@ -1,14 +1,6 @@
 #pragma once
-
 #include <stdio.h>
 #include <stdbool.h>
-
-
-typedef struct{
-	int StartIndex;
-	int EndIndex;
-	int NumOfReadingsInRange;
-}RangeReading;
 
 extern int numberofContinuousRange( int *InputReadings, int NumberofReadings);
 extern bool validateInput(int *InputSequence , int length);
