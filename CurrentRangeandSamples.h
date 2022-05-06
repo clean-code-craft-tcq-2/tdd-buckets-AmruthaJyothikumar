f40050a5b17f4af75e743abf1f9219ee8d56d2eb
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-06 16:23:43 +0530

    Update CurrentRangeandSamples.h

--- CurrentRangeandSamples.h
+++ CurrentRangeandSamples.h
@@ -8,7 +8,7 @@ typedef struct{
 	int NumOfReadingsInRange;
 }RangeReading;
 
-extern int numberofContinuousRange( int *InputReadings, int NumberofReadings);
+extern int findContinuousRangeCount( int *InputReadings, int NumberofReadings);
 extern bool validateInput(int *InputSequence , int length);
 extern void sortInput(int *InputSequence , int length);
 RangeReading detectRangeandOutputInCSVFormat( int min, int max, int numberInRange);
