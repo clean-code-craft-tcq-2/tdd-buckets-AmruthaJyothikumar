d01af2aec95d6c374eb60cbfbf01154140c24f29
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-08 11:09:15 +0530

    Update ADCInput.h

--- ADCInput.h
+++ ADCInput.h
@@ -4,6 +4,6 @@
 
 int maxAnalogToDigitalConversion(int ADC_Resolution);
 int stdAnalogToDigitalConvertedValue(int ADC_Resolution);
-void currentSenseAtoDConversion(int ADCValues[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentConversionValues);
+void ConvertAnalogToDigital(int ADCValues[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentConversionValues);
 void ignoreError(void);
 void ChargingCurrentSenseValue(int ADCValues[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentSenseValues);
