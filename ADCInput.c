a251ee94ae728cc4c0bc71b69f26999e5435b394
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-05-08 11:09:13 +0530

    Update ADCInput.c

--- ADCInput.c
+++ ADCInput.c
@@ -5,29 +5,29 @@ int maxAnalogToDigitalConversion(int ADC_Resolution)
 	return (pow(2,ADC_Resolution) - 2);
 }
 
-void currentSenseAtoDConversion(int ADCValues[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentConversionValues)
+void ConvertAnalogToDigital(int ADCInput[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentConversionValues)
 {	
-	float currentValue, analogToDigitalCoversionValue;
+	float currentValue, ConversionFactor;
 	
 	for (int i=0; i< numberOfSamples; i++)
 	{
-		analogToDigitalCoversionValue = (ADCValues[i]) / (maxAnalogToDigitalConversion(ADC_Resolution));
-		currentValue = (maxCurrentValue * analogToDigitalCoversionValue) / (maxAnalogToDigitalConversion(ADC_Resolution));
+		ConversionFactor = (ADCInput[i]) / (maxAnalogToDigitalConversion(ADC_Resolution));
+		currentValue = (maxCurrentValue * ConversionFactor) / (maxAnalogToDigitalConversion(ADC_Resolution));
 		currentConversionValues[i] = round(currentValue);
 		if(currentConversionValues[i] < 0)
 			currentConversionValues[i] = abs(currentConversionValues[i]);
 	}	
 }
 
-void ChargingCurrentSenseValue(int ADCValues[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentSenseValues)
+void ChargingCurrentSenseValue(int ADCInput[], int numberOfSamples, int ADC_Resolution, int maxCurrentValue, int* currentSenseValues)
 {
 	int errorValue;
 	errorValue = (pow(2,ADC_Resolution) - 1);
 	for (int i=0; i< numberOfSamples; i++)
 	{
-		if(ADCValues[i] != errorValue)
+		if(ADCInput[i] != errorValue)
 		{
-			currentSenseAtoDConversion(ADCValues, numberOfSamples, ADC_Resolution, maxCurrentValue,currentSenseValues);
+			ConvertAnalogToDigital(ADCInput, numberOfSamples, ADC_Resolution, maxCurrentValue,currentSenseValues);
 		}
 	}
 	
